fadb2528397c29c74774ea13beb0eac4c1022871
Anastasiya Popova <asya-nastya.92@mail.ru>
2020-08-24 21:45:55 +0300

    es-features-task8

--- src/es-features/task8.js
+++ src/es-features/task8.js
@@ -28,4 +28,24 @@ export function task8Old() {
 // Напишите реализацию функции task8Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task8New() {}
+export function task8New() {
+  let obj = {
+    nums: [0, 2, 5, 10, 15],
+    fives: [],
+    calculateNumsFive() 
+    {
+           for (let v of this.nums)
+      { 
+          if (v % 5 === 0) {
+          this.fives.push(v);
+         
+        }
+      };
+
+      return this.fives;
+    },
+  };
+
+  return obj.calculateNumsFive();
+  
+}
